bc480e92be2fe6de273f538163f3139b874abdb3
JMjirapat <jm_jirapat@hotmail.com>
2021-12-15 04:29:59 +0700

    All complete

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 int main (){
+	string textline;
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source, textline)){
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
+
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<string>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int numLine = 0;
+    double mean, stdDev, sum = 0, sum2 = 0;
+    string textline;
+    ifstream source;
+    source.open("score.txt");
+    while(getline(source, textline)){
+        sum += atof(textline.c_str());
+        sum2 += atof(textline.c_str())*atof(textline.c_str());;
+		numLine++;
+	}
+    mean = sum/numLine;
+    stdDev = sqrt(((1.0/numLine)*(sum2))-(mean*mean));
+    source.close();
+    cout << "Number of data = " << numLine << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = "<< stdDev;
+    return 0;
+}
+
